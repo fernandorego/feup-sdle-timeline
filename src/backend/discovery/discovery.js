685ea84d690884873fe4e2c9b2c80c9d60bf315b
Bruno Gomes <bruno.camgomes@gmail.com>
2022-12-05 10:44:28 +0000

    Remove MULTI_ADDRESS

--- src/backend/discovery/discovery.js
+++ src/backend/discovery/discovery.js
@@ -2,13 +2,10 @@ const LIB_P2P = require('libp2p')
 const TCP = require('libp2p-tcp')
 const DHT = require('libp2p-kad-dht')
 const MPLEX = require('libp2p-mplex')
-const MDNS = require('libp2p-mdns')({init: MULTI_ADDRESS});
+const MDNS = require('libp2p-mdns')
 const SECIO = require('libp2p-secio')
 const PEER_ID = require('peer-id');
-async function startNode(port) {
-    
-    MDNS.multiaddrs = [MULTI_ADDRESS];
-    
+async function startNode(port) {    
     
     node = await LIB_P2P.create({
         peerId: await PEER_ID.create({ bits: 1024 }),
@@ -29,7 +26,6 @@ async function startNode(port) {
                 mdns: { 
                     interval: 20e3,
                     enabled: true,
-                    ip : MULTI_ADDRESS
                 },
             },
             dht: {
