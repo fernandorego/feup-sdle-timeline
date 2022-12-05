d4c3c783df9fd94074ed19a43b82b23b6e88ed21
Bruno Gomes <bruno.camgomes@gmail.com>
2022-12-05 15:14:50 +0000

    Add bootstrap method

---
+++ src/backend/discovery/bootstrap.js
@@ -0,0 +1,5 @@
+export default bootstrapperNodes  = [
+    '/ip4/0.0.0.0/tcp/8001/p2p/QmP5UCpaCoESEdGxNeEhbgSgHuaQbXZnvn2mwhSgYpEbA8',
+		'/ip4/0.0.0.0/tcp/4001/p2p/QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N',
+		'/ip4/0.0.0.0/tcp/4001/p2p/QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx4N',
+];
--- src/backend/discovery/discovery.js
+++ src/backend/discovery/discovery.js
@@ -5,9 +5,12 @@ const MPLEX = require("libp2p-mplex");
 const MDNS = require("libp2p-mdns");
 const SECIO = require("libp2p-secio");
 const PEER_ID = require("peer-id");
+const BOOTSTRAP = require("libp2p-bootstrap");
 
 async function startNode(port) {
+	const bootstraperNodes = require("./bootstrap.js");
 	node = await LIB_P2P.create({
+
 		peerId: await PEER_ID.create({ bits: 1024 }),
 		addresses: {
 			listen: ["/ip4/0.0.0.0/tcp/" + (port + 1)],
@@ -16,12 +19,16 @@ async function startNode(port) {
 			transport: [TCP],
 			streamMuxer: [MPLEX],
 			connEncryption: [SECIO],
-			peerDiscovery: [MDNS],
+			peerDiscovery: [BOOTSTRAP, MDNS],
 			dht: DHT,
 		},
 		config: {
 			peerDiscovery: {
 				autoDial: true,
+				[BOOTSTRAP.tag]: {
+					enabled: true,
+					list: bootstraperNodes,
+				},
 				mdns: {
 					interval: 20e3,
 					enabled: true,
@@ -34,7 +41,7 @@ async function startNode(port) {
 	});
 
 	node.on("peer:discovery", (peerId) => {
-		console.log("Discovered:", peerId.toB58String());
+		//console.log("Discovered:", peerId.toB58String());
 	});
 	node.connectionManager.on("peer:connect", (connection) => {
 		console.log(
