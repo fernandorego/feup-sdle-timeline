3f94a05bc075092745555a890ba2580ffb78899b
fernandorego <fernandoluisrego@gmail.com>
2022-12-06 12:26:16 +0000

    Fix new node ip

--- src/backend-python/main.py
+++ src/backend-python/main.py
@@ -5,7 +5,7 @@ import asyncio
 from kademlia.network import Server
 
 BOOTSTRAP_IP = "127.0.0.1"
-BOOTSTRAP_PORT = 8468
+BOOTSTRAP_PORT = 8000
 
 handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -21,7 +21,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
 
     # Optional arguments
-    parser.add_argument("-i", "--ip", help="IP address of new  node", type=str, default=None)
+    parser.add_argument("-i", "--ip", help="IP address of new node", type=str, default=None)
     parser.add_argument("-p", "--port", help="port number of new node", type=int, default=None)
 
     return parser.parse_args()
@@ -31,7 +31,7 @@ def connect_to_bootstrap_node(args):
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
 
-    loop.run_until_complete(server.listen(8469))
+    loop.run_until_complete(server.listen(int(args.port)))
     bootstrap_node = (BOOTSTRAP_IP, BOOTSTRAP_PORT)
     loop.run_until_complete(server.bootstrap([bootstrap_node]))
 
